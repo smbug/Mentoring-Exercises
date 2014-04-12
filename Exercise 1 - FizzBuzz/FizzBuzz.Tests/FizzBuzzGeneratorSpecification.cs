dfe8a17f8fec24bde69b6f57f16489cd05a83c52
neal <neal.blomfield@gmail.com>
2014-04-13 07:13:06 +1200

    Added skeleton exercise code

---
+++ Exercise 1 - FizzBuzz/FizzBuzz.Tests/FizzBuzzGeneratorSpecification.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using FizzBuzz;
+
+namespace FizzBuzz.Tests
+{
+    [TestFixture]
+    public class FizzBuzzGeneratorSpecification
+    {
+        [Test]
+        public void FizzBuzz_Returns_UpperLimit_Items()
+        {
+            var fizzbuzz = new FizzBuzzGenerator();
+
+            Assert.That(fizzbuzz.FizzBuzz(10).Count(), Is.EqualTo(10), "Expected FizzBuzz( 10 ) to return 10 items"); 
+        }
+    }
+}
---
+++ Exercise 1 - FizzBuzz/FizzBuzz/FizzBuzzGenerator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FizzBuzz
+{
+    public class FizzBuzzGenerator
+    {
+        public IEnumerable<string> FizzBuzz(int upperLimit)
+        {
+            return Enumerable.Empty<string>();
+        }
+    }
+}
