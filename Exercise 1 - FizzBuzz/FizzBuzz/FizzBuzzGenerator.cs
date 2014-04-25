9852ef00dc9ba8ec1b6be65c0bea2d978cc03fdc
Sam <smgrc.reg@gmail.com>
2014-04-25 19:56:40 +1200

    Implement simplest solution for the FizzBuzz method to make the FizzBuzz_Returns_UpperLimit_Items unti test pass.

--- Exercise 1 - FizzBuzz/FizzBuzz/FizzBuzzGenerator.cs
+++ Exercise 1 - FizzBuzz/FizzBuzz/FizzBuzzGenerator.cs
@@ -5,11 +5,32 @@ using System.Text;
 
 namespace FizzBuzz
 {
+    /// <summary>
+    /// Simple "FizzBuzz" generator.
+    /// </summary>
     public class FizzBuzzGenerator
     {
+        /// <summary>
+        /// Generates the following "FizzBuzz" strings for each integer between 1 and a given number:
+        /// <list type="bullet">
+        /// <item><description>"Fizz" if the number is divisible by 3</description></item> 
+        /// <item><description>"Buzz" if the number is divisible by 5</description></item> 
+        /// <item><description>"FizzBuzz" if the number is divisible by 3 and 5</description></item> 
+        /// </list> 
+        /// </summary>
+        /// <param name="upperLimit">Upper limit...</param>
+        /// <returns>An empty enumeration, if <paramref name="upperLimit"/> is less than 1. 
+        /// Otherwise, an enumeration of "FizzBuzz" strings.</returns>
         public IEnumerable<string> FizzBuzz(int upperLimit)
         {
-            return Enumerable.Empty<string>();
+            var results = new List<string>();
+
+            for (int i = 1; i <= upperLimit; i++)
+            {
+                results.Add("Fizz");
+            }
+
+            return results;
         }
     }
 }
