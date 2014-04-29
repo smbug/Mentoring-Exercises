4037c1dbd8b64ba634b2c24977b7acd9fff3055c
Sam <smgrc.reg@gmail.com>
2014-04-29 22:07:45 +1200

    Rewrite method comment for FizzBuzz.

--- Exercise 1 - FizzBuzz/FizzBuzz/FizzBuzzGenerator.cs
+++ Exercise 1 - FizzBuzz/FizzBuzz/FizzBuzzGenerator.cs
@@ -11,17 +11,17 @@ namespace FizzBuzz
     public class FizzBuzzGenerator
     {
         /// <summary>
-        /// Generates the following "FizzBuzz" strings for each integer between 1 and a given number:
+        /// Generates one of the following "FizzBuzz" strings for each integer between 1 and a given number:
         /// <list type="bullet">
-        /// <item><description>"Fizz" if the integer is divisible by 3,</description></item> 
-        /// <item><description>"Buzz" if the integer is divisible by 5,</description></item> 
-        /// <item><description>"FizzBuzz" if the integer is divisible by 3 and 5, or</description></item> 
-        /// <item><description>the integer itself</description></item>
+        /// <item><description>"Fizz" if the integer is divisible by 3</description></item> 
+        /// <item><description>"Buzz" if the integer is divisible by 5</description></item> 
+        /// <item><description>"FizzBuzz" if the integer is divisible by 3 and 5</description></item> 
+        /// <item><description>Otherwise, the integer itself</description></item>
         /// </list> 
         /// </summary>
-        /// <param name="upperLimit">Upper limit of integer sequence used to generate the "FizzBuzz" strings</param>
-        /// <returns>An empty enumeration, if <paramref name="upperLimit"/> is less than 1. 
-        /// Otherwise, an enumerable of "FizzBuzz" strings.</returns>
+        /// <param name="upperLimit">Upper limit of the integer sequence used to generate the "FizzBuzz" strings</param>
+        /// <returns>An empty IEnumerable collection, if <paramref name="upperLimit"/> is less than 1. 
+        /// Otherwise, an IEnumerable collection of "FizzBuzz" strings.</returns>
         public IEnumerable<string> FizzBuzz(int upperLimit)
         {
             if (upperLimit <= 0)
