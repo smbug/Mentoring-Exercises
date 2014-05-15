2e812b189d44f7cc8960566bc3c4ae6a34a4b81f
Sam <smgrc.reg@gmail.com>
2014-05-15 22:27:01 +1200

    Base for Exercise 2 - Binary Search

---
+++ Exercise 2 - Binary Search/BinarySearch.Tests/BinarySearchSpecification.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BinarySearch.Tests
+{
+    [TestClass]
+    public class BinarySearchSpecification
+    {
+        [TestMethod]
+        public void Searching_an_empty_array_returns_null()
+        {
+            var emptyArray = new int[0];
+
+            Assert.IsNull(ExecuteFind(emptyArray, 1));
+        }
+
+        private int? ExecuteFind( int[] array, int value )
+        {
+            var searcher = new BinarySearch();
+            return searcher.Find(array, value);
+        }
+    }
+}
---
+++ Exercise 2 - Binary Search/BinarySearch/BinarySearch.cs
@@ -0,0 +1,24 @@
+﻿namespace BinarySearch
+{
+    public class BinarySearch
+    {
+        public int? Find( int[] a, int v )
+        {
+            var x = 0;
+            var y = a.Length;
+
+            while (x != y)
+            {
+                var m = (x + y) / 2;
+                var n = a[m];
+
+                if (n < v)
+                    x = m;
+                else
+                    y = m;
+            }
+
+            return a[x] == v ? x : (int?)null;
+        }
+    }
+}
