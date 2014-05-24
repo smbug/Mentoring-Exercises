8c03ed43f16962c27ed99b84a1e8ee28ac80bb16
Sam <smgrc.reg@gmail.com>
2014-05-24 18:19:30 +1200

    Throw exception when an unsorted array is passed to the Find method. Fulfills unit tests created in the previous revision.

--- Exercise 2 - Binary Search/BinarySearch/BinarySearch.cs
+++ Exercise 2 - Binary Search/BinarySearch/BinarySearch.cs
@@ -9,25 +9,33 @@ namespace BinarySearch
     public class BinarySearch
     {
         /// <summary>
-        /// Searches an array of integers for a specific number.
+        /// Searches an array of integers, sorted in ascending order, for a specific number.
         /// </summary>
         /// <param name="data">Array to search through</param>
         /// <param name="target">Number to find</param>
         /// <returns>If the number exists in the array, the index of the target number. Otherwise, null.</returns>
         /// <exception cref="ArgumentNullException">Occurs when <paramref name="data"/> is null.</exception>
+        /// <exception cref="ArgumentException">Occurs when <paramref name="data"/> is not sorted in ascending order</exception>
         public int? Find(int[] data, int target)
         {
             if (data == null)
             {
                 throw new ArgumentNullException("data");
             }
+
+            if (!CheckArrayIsSorted(data))
+            {
+                // NOTE The array isn't being returned at the end of the method, so the calling method
+                // may expect it to be in the same order as it was when it entered the method.
+                // So, it seems better to throw an exception for an unsorted array, rather than to sort it,
+                // to avoid unexpected surprises for the calling method.
+                throw new ArgumentException("Array must be sorted in ascending order", "data");
+            }
             
             if (data.Length == 0)
             {
                 return null;
             }
-
-            Array.Sort(data);
 
             var min = 0;
             var max = data.Length - 1;
@@ -52,6 +60,19 @@ namespace BinarySearch
             }
 
             return null;
+        }
+
+        private static bool CheckArrayIsSorted(int[] data)
+        {
+            for (int i = 1; i < data.Length; i++)
+            {
+                if (data[i - 1] > data[i])
+                {
+                    return false;
+                }
+            }
+
+            return true;
         }
     }
 }
