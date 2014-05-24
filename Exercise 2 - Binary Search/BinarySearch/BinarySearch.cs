bdd446ed917087ee30449c59287eb519ceb1300f
Sam <smgrc.reg@gmail.com>
2014-05-24 23:03:09 +1200

    Add analysis for handling duplicate values.

--- Exercise 2 - Binary Search/BinarySearch/BinarySearch.cs
+++ Exercise 2 - Binary Search/BinarySearch/BinarySearch.cs
@@ -5,6 +5,41 @@ namespace BinarySearch
 {
     /// <summary>
     /// Simple binary search.
+    /// 
+    /// NOTE
+    /// There is no "best" way to handle duplicate values; it all depends on what the search is required for.
+    /// Below are a few ways to handle duplicate values:
+    /// 
+    /// 1. Check that the array has distinct values, and throw an exception if it does not.
+    /// PROS: Useful for finding a value in a collection of keys.
+    /// CONS: Seems drastic to throw an exception when changing the collection type to a HashSet
+    /// will enforce the uniqueness constraint.
+    /// 
+    /// 2. Change the method signature to accept a HashSet. 
+    /// PROS: Useful for finding a value in a collection of keys. Also, since a HashSet will always have 
+    /// unique values, it will never need to be checked for duplicate values before the search begins.
+    /// CONS: Constrained to collection of distinct values.
+    /// 
+    /// 3. Change the method signature to return a boolean, and use the basic binary search algorithm.
+    /// PROS: Useful for confirming the existence of a value in a collection, much like a "Contains" method.
+    /// CONS: Will not return the position of a target.
+    /// 
+    /// 4. Allow arrays with duplicate values, use the basic binary search algorithm, and explain in the
+    /// method comment that the index of any instance of the target will be returned.
+    /// PROS: Useful for situations where the position of a target is not important.
+    /// CONS: Its 'pro' is also its downfall; method is not useful for finding specific positions.
+    /// 
+    /// 5. Modify the binary search algorithm to return the first index of the target, if it exists. For 
+    /// example, given {0, 2, 2, 2, 2, 3}, the algorithm will return 1, when searching for 2.
+    /// PROS: Useful for situations where the first position of a target is important.
+    /// CONS: Best case will be O(log(n)), unlike the basic binary search algorithm, as the entire array will 
+    /// need to be searched before stopping.
+    /// 
+    /// 6. Modify the binary search algorithm to return the first and last index of the target, if it exists.
+    /// For example, given {0, 2, 2, 2, 2, 3}, the algorithm will return 1 and 4, when searching for 2.
+    /// PROS: Useful for situations where the spread of a target needs to be found.
+    /// CONS: Best case will be O(log(n)), unlike the basic binary search, as the entire array will need to be
+    /// searched before stopping.
     /// </summary>
     public class BinarySearch
     {
@@ -15,7 +50,8 @@ namespace BinarySearch
         /// <param name="target">Number to find</param>
         /// <returns>If the number exists in the array, the index of the target number. Otherwise, null.</returns>
         /// <exception cref="ArgumentNullException">Occurs when <paramref name="data"/> is null.</exception>
-        /// <exception cref="ArgumentException">Occurs when <paramref name="data"/> is not sorted in ascending order</exception>
+        /// <exception cref="ArgumentException">Occurs when <paramref name="data"/> is not sorted in 
+        /// ascending order</exception>
         public int? Find(int[] data, int target)
         {
             if (data == null)
